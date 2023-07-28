764d64325edd9c671490eb069730dddbbe2a91f9
aidn5 <aydin.atrakchi@gmail.com>
2023-07-28 16:13:26 +0200

    add examples for a custom endpoint and a custom response

--- src/Custom.go
+++ src/Custom.go
@@ -1,5 +1,36 @@
 package src
 
-func Transform(request Request, response *Response) {
+import (
+	"encoding/json"
+	"net/http"
+)
 
+// Serve This function is only called on non-cached valid requests
+// with a positive code 200 response
+func Serve(request Request, response *Response, hypixel Hypixel) {
+	// example code
+
+	// custom endpoint
+	// declared in "endpoints.json"
+	if request.Path == "/example" {
+		response.Code = 200
+		response.Cache = true
+		response.Data = "{\"success\": true, \"example\": \"This is a custom example endpoint\"}"
+	}
+
+	// custom response for an existing endpoint
+	if request.Path == "/skyblock/profiles" {
+		var decoded map[string]any
+
+		err := json.Unmarshal([]byte(response.Data), &decoded)
+		if err != nil {
+			response.Code = http.StatusInternalServerError
+			response.Data = "{\"success\": false, \"cause\": \"could not parse hypixel json?\"}"
+			return
+		}
+
+		decoded["info"] = "This is an extra field in the response"
+		encoded, _ := json.Marshal(decoded)
+		response.Data = string(encoded)
+	}
 }
